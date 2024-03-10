7c5d47f2923575484eeb192c1381137950254601
salledelavage <salledelavager@gmail.com>
2024-03-10 10:07:23 -0400

    restart display manager

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -49,8 +49,8 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 
   # Enable OpenGL
