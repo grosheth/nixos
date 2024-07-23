74faf217980de7641a099420cb306e8a4eb1000e
salledelavage <salledelavager@gmail.com>
2024-07-23 13:26:23 -0400

    test

--- home-manager/rofi.nix
+++ home-manager/rofi.nix
@@ -13,6 +13,25 @@
       show-icons = true;
       display-drun = "";
       disable-history = false;
+      # combi-modi = "window,drun,ssh,Project:~/nixos/home-manager/packages/tools/dev";
+      # modi = "combi";
     };
   };
 }
+
+
+# configuration {
+#     combi-modi: "window,drun,ssh,Project:~/.config/rofi/project.sh";
+#     modi: "combi";
+# }
+
+#!/usr/bin/env sh
+
+# List projects if no arguments given
+# if [ -z "$1" ]; then
+#     ghq list
+# else
+#     code "$HOME/p/$1"
+# fi
+
+# Now, rofi -show combi should open with a default combi mode+project launcher.
