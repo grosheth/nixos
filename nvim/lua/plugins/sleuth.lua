8929313843c7a8c122e8bdf155712d31f995145a
salledelavage <salledelavager@gmail.com>
2024-08-30 08:25:26 -0400

    Redo nvim config

---
+++ nvim/lua/plugins/sleuth.lua
@@ -0,0 +1,5 @@
+return {
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  -- Use the `dependencies` key to specify the dependencies of a particular plugin
+  -- LSP Plugins
+}
