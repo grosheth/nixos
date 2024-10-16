ae455e8e82396867417976d23a021dc9b5ac115d
salledelavage <salledelavager@gmail.com>
2024-10-16 08:09:02 -0400

    adding neovim default colorscheme

--- home-manager/starship.nix
+++ home-manager/starship.nix
@@ -8,21 +8,34 @@ let
   };
   langs = "$nodejs$python$rust$golang$lua";
   colors = {
-    black = "#212026";
-    pink = "#cea2ca";
-    red = "#e55c74";
-    green = "#6dd797";
-    purple = "#c678dd";
-    yellow = "#eed891";
-    cyan = "#56b6c2";
-    blue = "#4fa6ed";
+    # kaolin
+    # black = "#212026";
+    # pink = "#cea2ca";
+    # red = "#e55c74";
+    # green = "#6dd797";
+    # purple = "#c678dd";
+    # yellow = "#eed891";
+    # cyan = "#56b6c2";
+    # blue = "#4fa6ed";
+    # blue_alt = "#0bc9cf";
+    # white = "#dcdfe4";
+    # fg = "#ffffff";
+    # bg = "#18181b";
+
+    # neovim
+    black = "#14161b";
+    pink = "#ffcaff";
+    red = "#ffc0b9";
+    green = "#b3f6c0";
+    purple = "#ffcaff";
+    yellow = "#fce094";
+    cyan = "#8cf8f7";
+    blue = "#a6dbff";
     blue_alt = "#0bc9cf";
-    white = "#dcdfe4";
-    #darker
-    fg = "#ffffff";
-    # bg = "#111111";
-    bg = "#18181b";
-    # fg = "#dcdfe4";
+    white = "#ffffff";
+    fg = "#eef1f8";
+    bg = "#14161b";
+
 };
 in
 {
