33fe848ddc99d5ffdbbb941632c699d7018a7e66
salledelavage <salledelavager@gmail.com>
2024-10-18 21:53:39 -0400

    remove colors

--- home-manager/zellij.nix
+++ home-manager/zellij.nix
@@ -6,33 +6,6 @@
     ];
   };
 
-  # kaolin
-  # black = "#212026";
-  # pink = "#cea2ca";
-  # red = "#e55c74";
-  # green = "#6dd797";
-  # purple = "#c678dd";
-  # yellow = "#eed891";
-  # cyan = "#56b6c2";
-  # blue = "#4fa6ed";
-  # blue_alt = "#0bc9cf";
-  # white = "#dcdfe4";
-  # fg = "#ffffff";
-  # bg = "#18181b";
-
-  # neovim
-  # black = "#14161b";
-  # pink = "#ffcaff";
-  # red = "#ffc0b9";
-  # green = "#b3f6c0";
-  # purple = "#ffcaff";
-  # yellow = "#fce094";
-  # cyan = "#8cf8f7";
-  # blue = "#a6dbff";
-  # blue_alt = "#0bc9cf";
-  # white = "#ffffff";
-  # fg = "#eef1f8";
-  # bg = "#14161b";
   programs.zellij = {
     enable = true;
     enableZshIntegration = true;
@@ -40,15 +13,27 @@
       theme = "kaolin";
       themes.kaolin.fg = "#e6e6e8";
       themes.kaolin.bg = "#18181b";
-      themes.kaolin.black = "#18181b";
-      themes.kaolin.red = "#18181b";
-      themes.kaolin.green = "#b3f6c0";
-      themes.kaolin.blue = "#a6dbff";
-      themes.kaolin.yellow = "#ffc0b9";
-      themes.kaolin.magenta = "#ffcaff";
-      themes.kaolin.cyan = "#8cf8f7";
+      themes.kaolin.black = "#212026";
+      themes.kaolin.red = "#e55c74";
+      themes.kaolin.green = "#6dd797";
+      themes.kaolin.blue = "#4fa6ed";
+      themes.kaolin.yellow = "#e55c74";
+      themes.kaolin.magenta = "#cea2ca";
+      themes.kaolin.cyan = "#56b6c2";
       themes.kaolin.white = "#eef1f8";
-      themes.kaolin.orange = "#8cf8f7";
+      themes.kaolin.orange = "#56b6c2";
+      # theme = "neovim";
+      # themes.neovim.fg = "#e6e6e8";
+      # themes.neovim.bg = "#18181b";
+      # themes.neovim.black = "#18181b";
+      # themes.neovim.red = "#18181b";
+      # themes.neovim.green = "#b3f6c0";
+      # themes.neovim.blue = "#a6dbff";
+      # themes.neovim.yellow = "#ffc0b9";
+      # themes.neovim.magenta = "#ffcaff";
+      # themes.neovim.cyan = "#8cf8f7";
+      # themes.neovim.white = "#eef1f8";
+      # themes.neovim.orange = "#8cf8f7";
     };
     # extraConfig = ''
     #     {
