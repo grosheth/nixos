fdd7e9c3aabfe9773cd82a0b62dae4182f1f5cc7
salledelavage <salledelavager@gmail.com>
2025-04-15 20:25:12 -0400

    testing multiple import

--- home-manager/gysmo.nix
+++ home-manager/gysmo.nix
@@ -1,56 +1,71 @@
 { config, pkgs, ... }:
+
 let
-    gysmo = pkgs.callPackage /home/salledelavage/work/nixpkgs/pkgs/by-name/gy/gysmo/package.nix {};
+  # Import the local nixpkgs repository
+  localPkgs = import /home/salledelavage/work/nixpkgs {};
+
+  # Import the local home-manager repository
+  homeManager = import /home/salledelavage/work/home-manager {
+    pkgs = localPkgs;
+    # Add any other necessary arguments here
+  };
 in
 {
-  programs.gysmo = {
-    enable = true;
-    package = gysmo;
-    config = {
-      items = [
-        {
-          text = "Item 1";
-          keyword = "kw1";
-          icon = "icon1";
-          value_color = "#ff0000";
-          text_color = "#00ff00";
-          icon_color = "#0000ff";
-        }
-        {
-          text = "Item 2";
-          value = "val2";
-          icon = "icon2";
-          value_color = "#ff00ff";
-          text_color = "#00ffff";
-          icon_color = "#ffff00";
-        }
-      ];
-      ascii = {
-        path = "/path/to/ascii";
-        colors = "#ffffff";
-        enabled = true;
-        horizontal_padding = 2;
-        vertical_padding = 2;
-        position = "center";
-      };
-      header = {
-        enabled = true;
-        line = "Header Line";
-        text = true;
-        text_color = "#ff00ff";
-        line_color = "#00ffff";
-      };
-      footer = {
-        enabled = true;
-        line = "Footer Line";
-        text = true;
-        text_color = "#ffff00";
-        line_color = "#ff00ff";
-      };
-      general = {
-        menu_type = "box";
-        columns = true;
-        menu_padding = 5;
+  # Use the home-manager module
+  imports = [
+    homeManager.home-manager.nixosModules.home-manager
+  ];
+
+  home-manager.users.salledelavage = { pkgs, ... }: {
+    programs.gysmo = {
+      enable = true;
+      package = pkgs.gysmo;
+      config = {
+        items = [
+          {
+            text = "Item 1";
+            keyword = "kw1";
+            icon = "icon1";
+            valueColor = "#ff0000";
+            textColor = "#00ff00";
+            iconColor = "#0000ff";
+          }
+          {
+            text = "Item 2";
+            value = "val2";
+            icon = "icon2";
+            valueColor = "#ff00ff";
+            textColor = "#00ffff";
+            iconColor = "#ffff00";
+          }
+        ];
+        ascii = {
+          path = "/path/to/ascii";
+          colors = "#ffffff";
+          enabled = true;
+          horizontalPadding = 2;
+          verticalPadding = 2;
+          position = "center";
+        };
+        header = {
+          enabled = true;
+          line = "Header Line";
+          text = true;
+          textColor = "#ff00ff";
+          lineColor = "#00ffff";
+        };
+        footer = {
+          enabled = true;
+          line = "Footer Line";
+          text = true;
+          textColor = "#ffff00";
+          lineColor = "#ff00ff";
+        };
+        general = {
+          menuType = "box";
+          columns = true;
+          menuPadding = 5;
+        };
       };
     };
   };
