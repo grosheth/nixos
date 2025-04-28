70067fe0731eaa9ec3e09e4637382ea1efb43e31
salledelavage <salledelavager@gmail.com>
2025-04-28 07:21:20 -0400

    test

--- home-manager/gysmo.nix
+++ home-manager/gysmo.nix
@@ -118,6 +118,120 @@
           };
         };
       }
+      {
+        name = "config2";
+        config = {
+          items = [
+            {
+              text = "User";
+              keyword = "user";
+              icon = "";
+              value_color = "red";
+              text_color = "";
+              icon_color = "red";
+            }
+            {
+              text = "Kernel";
+              value = "kernel";
+              icon = "󰌽";
+              value_color = "";
+              text_color = "";
+              icon_color = "";
+            }
+            {
+              text = "WM";
+              value = "wm";
+              icon = "󱂬";
+              value_color = "yellow";
+              text_color = "";
+              icon_color = "yellow";
+            }
+            {
+              text = "GPU";
+              value = "gpu";
+              icon = "";
+              value_color = "green";
+              text_color = "";
+              icon_color = "green";
+            }
+            {
+              text = "Term";
+              value = "term";
+              icon = "";
+              value_color = "blue";
+              text_color = "";
+              icon_color = "blue";
+            }
+            {
+              text = "CPU";
+              value = "cpu";
+              icon = "";
+              value_color = "red";
+              text_color = "";
+              icon_color = "red";
+            }
+            {
+              text = "OS";
+              value = "NixOs";
+              icon = "";
+              value_color = "";
+              text_color = "";
+              icon_color = "";
+            }
+            {
+              text = "RAM";
+              value = "ram";
+              icon = "";
+              value_color = "yellow";
+              text_color = "";
+              icon_color = "yellow";
+            }
+            {
+              text = "Shell";
+              value = "zsh";
+              icon = "";
+              value_color = "green";
+              text_color = "";
+              icon_color = "green";
+            }
+            {
+              text = "Uptime";
+              value = "uptime";
+              icon = "󱑆";
+              value_color = "blue";
+              text_color = "";
+              icon_color = "blue";
+            }
+          ];
+          ascii = {
+            path = "ascii/gysmo";
+            colors = "#ffffff";
+            enabled = true;
+            horizontal_padding = 2;
+            vertical_padding = 2;
+            position = "top";
+          };
+          header = {
+            enabled = false;
+            line = true;
+            text = "";
+            text_color = "";
+            line_color = "";
+          };
+          footer = {
+            enabled = false;
+            line = true;
+            text = "";
+            text_color = "";
+            line_color = "";
+          };
+          general = {
+            menu_type = "box";
+            columns = true;
+            menu_padding = 5;
+          };
+        };
+      }
     ];
     asciiFiles = [
       { name = "gysmo"; content = ''
