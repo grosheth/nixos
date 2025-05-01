2ce72f50c8ce89975a703fa12b9d4b34e097b69a
salledelavage <salledelavager@gmail.com>
2025-05-01 07:58:03 -0400

    tets

--- home-manager/gysmo-backup.nix
+++ home-manager/gysmo-backup.nix
@@ -16,6 +16,78 @@
               text_color = "";
               icon_color = "red";
             }
+            {
+              text = "Kernel";
+              value = "kernel";
+              icon = "󰌽";
+              value_color = "";
+              text_color = "";
+              icon_color = "";
+            }
+            {
+              text = "WM";
+              value = "wm";
+              icon = "󱂬";
+              value_color = "yellow";
+              text_color = "";
+              icon_color = "yellow";
+            }
+            {
+              text = "GPU";
+              value = "gpu";
+              icon = "";
+              value_color = "green";
+              text_color = "";
+              icon_color = "green";
+            }
+            {
+              text = "Term";
+              value = "term";
+              icon = "";
+              value_color = "blue";
+              text_color = "";
+              icon_color = "blue";
+            }
+            {
+              text = "CPU";
+              value = "cpu";
+              icon = "";
+              value_color = "red";
+              text_color = "";
+              icon_color = "red";
+            }
+            {
+              text = "OS";
+              value = "NixOs";
+              icon = "";
+              value_color = "";
+              text_color = "";
+              icon_color = "";
+            }
+            {
+              text = "RAM";
+              value = "ram";
+              icon = "";
+              value_color = "yellow";
+              text_color = "";
+              icon_color = "yellow";
+            }
+            {
+              text = "Shell";
+              value = "zsh";
+              icon = "";
+              value_color = "green";
+              text_color = "";
+              icon_color = "green";
+            }
+            {
+              text = "Uptime";
+              value = "uptime";
+              icon = "󱑆";
+              value_color = "blue";
+              text_color = "";
+              icon_color = "blue";
+            }
           ];
           ascii = {
             path = "ascii/gysmo";
