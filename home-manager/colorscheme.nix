e673311eccf2fb6fe9c4009eb7c39d1ef675d93a
salledelavage <salledelavager@gmail.com>
2026-09-22 12:14:53 -0400

    change colorscheme

--- home-manager/colorscheme.nix
+++ home-manager/colorscheme.nix
@@ -2,32 +2,76 @@
 
 let
   color = name: hex: { inherit name hex; };
-  selected = "medieval";
-  schemes = {
-    medieval = import ./colorschemes/medieval.nix { inherit color; };
-    everforest = {
-      name = "Everforest";
-      fg = color "foreground" "#D3C6AA";
-      bg = color "background" "#14161b";
-      foreground = color "foreground" "#D3C6AA";
-      background = color "background" "#14161b";
-      selection_foreground = color "selection_forground" "#000000";
-      selection_background = color "selection_background" "#FFFACD";
-      cursor = color "cursor" "#ffffff";
-      black = color "black" "#212026";
-      red = color "red" "#E67E80";
-      green = color "green" "#A7C080";
-      yellow = color "yellow" "#DBBC7F";
-      blue = color "blue" "#7FBBB3";
-      blue_alt = color "blue_alt" "#0bc9cf";
-      magenta = color "magenta" "#D699B6";
-      cyan = color "cyan" "#83C092";
-      purple = color "purple" "#E69875";
-      white = color "white" "#D3C6AA";
-    };
-  };
 in
-schemes.${selected}
+# medieval
+{
+  name = "Medieval";
+  fg = color "foreground" "#D6C7AE";
+  bg = color "background" "#10141B";
+  foreground = color "foreground" "#D6C7AE";
+  background = color "background" "#10141B";
+  background_dark = color "background_dark" "#090603";
+  background_alt = color "background_alt" "#1B1E23";
+  surface = color "surface" "#262931";
+  surface_raised = color "surface_raised" "#293342";
+  surface_blue = color "surface_blue" "#323B4C";
+  foreground_bright = color "foreground_bright" "#E7DCC6";
+  foreground_muted = color "foreground_muted" "#9B8D7D";
+  disabled = color "disabled" "#62594F";
+
+  selection_foreground = color "selection_foreground" "#10141B";
+  selection_background = color "selection_background" "#D08A43";
+  cursor = color "cursor" "#E0A15F";
+
+  gold = color "gold" "#B7782F";
+  amber = color "amber" "#D08A43";
+  bright_amber = color "bright_amber" "#E0A15F";
+  firelight = color "firelight" "#CB8D59";
+  sunset_rust = color "sunset_rust" "#A85E46";
+
+  black = color "black" "#1B1E23";
+  red = color "red" "#A85E46";
+  green = color "green" "#6F845B";
+  yellow = color "yellow" "#B98A4A";
+  blue = color "blue" "#607695";
+  magenta = color "magenta" "#806C85";
+  cyan = color "cyan" "#718995";
+  white = color "white" "#C5B7A0";
+
+  bright_black = color "bright_black" "#515561";
+  bright_red = color "bright_red" "#CE7860";
+  bright_green = color "bright_green" "#8EA477";
+  bright_yellow = color "bright_yellow" "#D6A75E";
+  bright_blue = color "bright_blue" "#8298B7";
+  bright_magenta = color "bright_magenta" "#A18AA5";
+  bright_cyan = color "bright_cyan" "#91A7B0";
+  bright_white = color "bright_white" "#E7DCC6";
+
+  blue_alt = color "blue_alt" "#8298B7";
+  purple = color "purple" "#CB8D59";
+}
+
+# everforest
+# {
+#   name = "Everforest";
+#   fg = color "foreground" "#D3C6AA";
+#   bg = color "background" "#14161b";
+#   foreground = color "foreground" "#D3C6AA";
+#   background = color "background" "#14161b";
+#   selection_foreground = color "selection_forground" "#000000";
+#   selection_background = color "selection_background" "#FFFACD";
+#   cursor = color "cursor" "#ffffff";
+#   black = color "black" "#212026";
+#   red = color "red" "#E67E80";
+#   green = color "green" "#A7C080";
+#   yellow = color "yellow" "#DBBC7F";
+#   blue = color "blue" "#7FBBB3";
+#   blue_alt = color "blue_alt" "#0bc9cf";
+#   magenta = color "magenta" "#D699B6";
+#   cyan = color "cyan" "#83C092";
+#   purple = color "purple" "#E69875";
+#   white = color "white" "#D3C6AA";
+# }
 
 # kaolin
 # {
