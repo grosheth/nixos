3a8824891b1d9931419c1d185fd10c7a7ed02fc1
salledelavage <salledelavager@gmail.com>
2026-09-22 11:19:03 -0400

    adding new wallpaper

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -297,7 +297,7 @@ in {
     configType = "lua";
     extraConfig = ''
     local mod = "ALT"
-    local terminal = "kitty"
+    local terminal = "ghostty"
 
     hl.monitor({ output = "HDMI-A-1", mode = "2560x1440@144", position = "0x0", scale = 1 })
     hl.monitor({ output = "DP-3", mode = "3440x1440@100", position = "2560x0", scale = 1 })
